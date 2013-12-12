161d1dc2535f9e832c9a57f94cdc804a4101b0db
web <web@mreider.(none)>
2013-12-12 05:51:06 +0000

    works, almost

--- goatrpc/views.py
+++ goatrpc/views.py
@@ -1,9 +1,9 @@
 from jsonrpc import jsonrpc_method
 
-@jsonrpc_method('goatrpc.sayHello')
+@jsonrpc_method('sayHello')
 def whats_the_time(request, name='Lester'):
   return "Hello %s" % name
 
-@jsonrpc_method('goatrpc.gimmeThat', authenticated=True)
+@jsonrpc_method('gimmeThat', authenticated=True)
 def something_special(request, secret_data):
   return {'sauce': ['authenticated', 'sauce']}
